b21b4dd08143081a9b151c21dcbc081ed254c2d1
5may-sytk <ec2-user@ip-172-31-18-97.eu-north-1.compute.internal>
2024-12-11 14:23:34 +0000

    12/11

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  post 'lists' => 'lists#create' 
+  get 'lists' => 'lists#index'
+  get 'homes/top'
+  get 'lists/:id' => 'lists#show' , as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
