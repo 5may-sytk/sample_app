5f3ccf8e4acb4e3a055cbdcb68a94b152a2f899e
5may-sytk <ec2-user@ip-172-31-18-97.eu-north-1.compute.internal>
2024-12-12 01:18:25 +0000

    ７章途中

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create' 
   get 'lists' => 'lists#index'
   get 'homes/top'
-  get 'lists/:id' => 'lists#show' , as: 'list'
+  get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
